45c969ff75ec2af5bef048ddfc3c46a651557fcd
Amir Lebba <Amirlebba@gmail.com>
2024-10-23 23:49:13 +0100

    Working on the phone styling

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+import Phone from "./Phone"
+function App() {
+  return (
+    <>  
+      <aside>
+        
+      </aside>
+      <main>
+        <section className='phone'>
+          <Phone/>
+        </section>
+        <section className='message'></section>
+      </main>
+    </>
+  );
+}
+
+export default App;
+
---
+++ src/Phone.jsx
@@ -0,0 +1,21 @@
+import './Phone.css'
+function Phone() {
+    return (
+        <>
+            <div className="body">
+                <div className="Notch"></div>
+                <div className="screen">
+                    <div className="navbar">
+                        <div className="arrow"></div>
+                        <div className="info"></div>
+                        <div className="dots"></div>
+                    </div>
+                    <div className="Text"></div>
+                    <div className="footer"></div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Phone ;
